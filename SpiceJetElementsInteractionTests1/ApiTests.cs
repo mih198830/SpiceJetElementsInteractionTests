843945b406709fa93844c3712ae5e17058e1724a
Mihail Matskevich <mih198830@gmail.com>
2023-02-21 14:41:02 +0100

    test added api

--- SpiceJetElementsInteractionTests1/ApiTests.cs
+++ SpiceJetElementsInteractionTests1/ApiTests.cs
@@ -34,12 +34,44 @@ namespace SpiceJetElementsInteractionTests1
                 Assert.IsTrue(resp.status.Equals("SUCCESS"));
             }
 
-            
+            private RestClient _client;
+
+            [SetUp]
+            public void Setup()
+            {
+                _client = new RestClient("https://www.spicejet.com/api/v3/search/availability");
+            }
+            [Test]
+            public void ShouldReturnAvailableFlights()
+            {
+                // Arrange
+                var request = new RestRequest("availability", Method.Post);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddJsonBody(new
+                {
+                    originStationCode = "DEL",
+                    destinationStationCode = "BOM",
+                    onWardDate = "2023 - 02 - 27",
+                    currency = "INR",
+                    pax = request.AddJsonBody(new
+                    {
+                        journeyClass = "ff",
+                        adult = 1,
+                        child = 0,
+                        infant = 0,
+                        srCitizen = 0
+                    })
+                });
+
+                // Act
+                RestResponse response = _client.Execute(request);
+
+                // Assert
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(response.Content, Does.Contain("data"));
+            }
         }
 
     }
-
-
-
 }
 
